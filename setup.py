dfc6b278cb50ece742abac901f6381ce7d4d97bb
Matthew J Morrison <mattj.morrison@gmail.com>
2010-11-05 23:21:29 -0500

    corrected description, this is not a django app

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 setup(
     name="royal-rubmle-twitter",
     version="0.1",
-    description="Django Twitter Clone for ICC Royal Rumble",
+    description="Python Twitter Clone for ICC Royal Rumble",
     author="Matthew J. Morrison",
     package_dir={'': 'src'},
     install_requires = (
-        'mock',
         'wsgiref',
         'pesto',
         'Jinja2',
